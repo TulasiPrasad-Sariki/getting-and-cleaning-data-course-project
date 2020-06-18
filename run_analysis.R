2d6f2479cee5ef3ca1732af8272200178342dc1e
TulasiPrasad-Sariki <66796554+TulasiPrasad-Sariki@users.noreply.github.com>
2020-06-18 14:06:54 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,11 @@
+# Author: Tulasi Prasad Sariki
+
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 library(reshape2)
 
 
@@ -16,7 +24,6 @@ if (!file.exists(dataDir)) {
     dir.create(dataDir)
     unzip(zipfile = rawDataDFn, exdir = dataDir)
 }
-
 
 #2. merge {train, test} data set
 # refer: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
